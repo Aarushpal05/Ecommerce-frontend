db2c41ea1095f99e10d5d11f73f881fe04bfbd92
Ayush pal <aarushpal876@gmail.com>
2026-06-17 13:13:47 +0530

    Update productaddtocard.js

--- src/pages/productaddtocard.js
+++ src/pages/productaddtocard.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const addToCart = (productId, navigate) => {
   const token = localStorage.getItem("access_token");
